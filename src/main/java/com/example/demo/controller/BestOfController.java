dff2e736751baae54d15e3f6902d7a199060875e
Valentina <valelore98@gmail.com>
2022-10-17 18:36:07 +0200

    Finished and tested controller

--- src/main/java/com/example/demo/controller/BestOfController.java
+++ src/main/java/com/example/demo/controller/BestOfController.java
@@ -6,19 +6,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Movie;
+import com.example.demo.model.Song;
 
 @RestController
 @RequestMapping("/bestof")
 public class BestOfController {
 	
 	@GetMapping("/movie")
-	public Movie bestMovie(@RequestParam(name = "prova", required = false)) {
-		return new Movie();
+	public Movie bestMovie(@RequestParam(name = "prova", required = false) String titolo) {
+		return new Movie("Harry Potter", "1998", "Chris Columbus", new String[]{"Daniel Radcliffe","Emma Watson", "Rupert Grint"});
 	}
 	
 	@GetMapping("/song")
-	public void bestSong(@RequestParam(name = "prova", required = false)) {
-		return new Song();
+	public Song bestSong(@RequestParam(name = "prova", required = false) String titolo) {
+		return new Song("Io vengo dalla luna", "2017", "Maneskin", "Rock", "Caparezza");
 	}
 
 	
